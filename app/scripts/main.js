6a6b457ac0f05c9eaa98c751cc3d4f775e6c7a44
Matt Bertino <mattbertino@gmail.com>
2014-06-30 18:50:50 -0400

    finished with the initial sign up page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,7 @@
-console.log('\'Allo \'Allo!');
+$(window).resize(function() {
+  $('.signup').css({
+    position: 'absolute',
+    left: ($(window).width() - $('.signup').outerWidth()) / 2,
+    top: ($(window).height() - $('.signup').outerHeight()) / 2
+  });
+});
