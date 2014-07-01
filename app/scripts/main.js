32b34de683e0e6ce2762f8a125cc8066e44b6756
Matt Bertino <mattbertino@gmail.com>
2014-07-01 16:07:13 -0400

    Distribution Commit

--- app/scripts/main.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  Parse.initialize("G9uyCX4XU9TeNlK68J7Twj2cB9r6uS2yaejr4WEM", "cEoLgevYZmA7OHALB8Z5dyZoGnyJ3MvdfVRIKDDv");
-
-  window.router = new MainRouter();
-  Backbone.history.start();
-});
